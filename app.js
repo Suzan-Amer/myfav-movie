7c58349482d4e35dfb6d71d3630ee1d9554134c9
SuzanAmer <>
2021-04-14 13:20:31 +0000

    java

---
+++ app.js
@@ -0,0 +1,31 @@
+// console.log('hi');
+alert ('Welcome to my site');
+
+var userName= prompt ('Please enter your name');
+console.log('user name');
+
+
+var userAge=prompt ('Please enter your age');
+if ( userAge >= 18){
+   alert ('Welcome you can watch the movie ; Enjoy');
+} else {
+  alert ('sorry you can not watch the movie because it contains violent scenes');
+}
+
+var timeNow= prompt('What time is it now please');
+if (timeNow >=12){
+  document.write('Good morning, hero');
+  document.write('<img src=https://image.freepik.com/free-vector/creative-good-morning-lettering-background_23-2148244893.jpg>');
+
+  } else if (timeNow >=24){
+  document.write('Good night hero');
+  document.write ('img src=https://alnqsh-prod.s3.eu-central-1.amazonaws.com/4199/conversions/Good-Night-medium.jpg');
+
+  }
+  
+  
+  
+
+
+
+
