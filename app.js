61414881313e77fe274c05ed975fa23b311656fd
SuzanAmer <>
2021-04-15 11:14:18 +0000

    kim

--- app.js
+++ app.js
@@ -25,3 +25,25 @@ if (timeNow >=12){
   
 
 
+var characterName = prompt('hello plz enter your fav char name from call move !');
+
+while (characterName !== 'park' && characterName !== 'kim' )
+{
+  characterName = prompt (' plz enter the character Name park or kim')
+}
+
+var userPhoto;
+
+if (characterName === 'park' ){
+userPhoto='<img src="https://www.asiaholic.net/wp-content/uploads/2015/01/rsz_park-shin-hye.jpg">';
+} else if (characterName === 'kim'){
+userPhoto='<img src="https://upload.wikimedia.org/wikipedia/commons/1/10/Oh_Se-hun_-_2016_Gaon_Chart_K-pop_Awards_red_carpet.jpg">';
+  
+}
+
+
+var imagesNumber = prompt ('how many image do you want?');
+
+for (var i=0 ; i < imagesNumber ; i++ ){
+  document.write(userPhoto);
+}
